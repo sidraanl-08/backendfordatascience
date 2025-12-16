d2c936c37ab2b1384e6f9817779bc0cfe2cf9b43
SidraNl <sidraa.nl08@gmail.com>
2025-12-16 12:31:47 +0100

    added notes  📃

--- crudapp.py
+++ crudapp.py
@@ -1,9 +1,19 @@
-from flask import Flask, request, jsonify
-import mysql.connector
+from flask import Flask, request, jsonify #request is to receive data sent by client #jsonify send data back to flask using dicts
+import mysql.connector #to connect flask with mysql database, we can use SQL queries within flask 
+
+
+#here Flask(__name__) is called flask application object
+#__name__ The current Python file is the main app
+#__name__ Helps Flask know where to look for templates, static files, etc.
+#app is the variable in which we store the flask application object FAO
+#we use same app variable to create routes, and run the server
 
 app = Flask(__name__)
 
+
 #this is mysql connection
+#it opens connection between flask and mysql
+#it helps flask read, fetch, retrieve data from database and send data into it
 
 db = mysql.connector.connect(
     host = 'localhost',
@@ -12,13 +22,27 @@ db = mysql.connector.connect(
     database = 'flaskcrud'
 )
 
+#cursor is a tool that flask uses to run SQL queries
+#it talks to the database and works per the flask requests being made (CREATE, READ, UPDATE, DELETE)
+#apis use json to work with data, so here dictionary = True means return data in form of a dictionary in json
+
 cursor = db.cursor(dictionary=True)
+
+#this is a test route to check whether flask app is working or not
 
 @app.route('/')
 def Index():
     return "<h1> Hello Flask Application </h1>"
 
-#make a new user 
+
+#CREATE Route:::: make a new user 
+#request.json reads data sent by client (postman) in json 
+#request.json is used when client sends data to flask (eg: in CREATE & UPDATE of CRUD)
+#cursor.execute performs SQL commands and follows flask instructions
+#db.commit will store the data changes 
+#jsonify will return the response to flask 
+#200, 201 are HTTP status codes, they tell us whether our request was successful or not
+
 
 @app.route('/user', methods=['POST'])
 def create_user():
@@ -31,7 +55,13 @@ def create_user():
     return jsonify({"message" : "user created :)"}), 201
 
 
-#read one user with fetchone
+
+#READ Route:::: read one user with fetchone()
+#<int:id> is a URL param where it picks data per given id converted into integer
+#(id,) is used coz SQL placeholders use a tuple so in python tuple is made with a comma
+#%s is a safe and reliable for safe value insertion and prevents SQL injection
+#fetchone gets only one record given through the URL param
+
 
 @app.route('/user/<int:id>', methods=['GET'])
 def get_user(id):
@@ -40,7 +70,11 @@ def get_user(id):
     return jsonify(user)
 
 
-#update user by id
+
+#UPDATE Route:::: update user by id
+#similar like CREATE Route 
+#only SQL operation is different, here users are being updated 
+
 
 @app.route('/user/<int:id>', methods=['PUT'])
 def update_user(id):
@@ -53,7 +87,11 @@ def update_user(id):
     return jsonify({'message' : 'user updated :)'})
 
 
-#delete user by id
+
+#DELETE Route:::: delete user by id
+#this is same as GET Route
+#SQL Operation is different, here users are being deleted
+
 
 @app.route('/user/<int:id>', methods=['DELETE'])
 def delete_user(id):
@@ -62,7 +100,8 @@ def delete_user(id):
     return jsonify ({'message': "user deleted :("})
 
 
-#read all users with fetchall
+#read all users with fetchall()
+#fetchall() is responsible to read/get/fetch all users from database
 
 @app.route('/users', methods=['GET'])
 def get_users():
@@ -72,7 +111,11 @@ def get_users():
 
 
 
-#run ur server
+#if line ensures that file is run directly and is not imported as module
+#once its made sure, only then it runs the flask server
+#app.run will start the flask's built-in development server
+#debug=True will show errors in console
+#debug=True also reloads the server if any code changes made 
 
 if __name__ == '__main__':
     app.run(debug=True)
